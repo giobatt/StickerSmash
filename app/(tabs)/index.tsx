e0e02cb875acc8fb4415b9b393524d3ee6c9b11c
Giorgio Battistoni <giobatt@gmail.com>
2025-02-04 17:39:38 +0100

    Capture a screenshot and save it

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,22 @@
-import * as MediaLibrary from "expo-media-library";
 import { View, StyleSheet } from "react-native";
-import ImageViewer from "@/components/ImageViewer";
-import Button from "@/components/Button";
 import * as ImagePicker from "expo-image-picker";
 import { useRef, useState } from "react";
-import CircleButton from "@/components/CircleButton";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as MediaLibrary from "expo-media-library";
+import { captureRef } from "react-native-view-shot";
+import { type ImageSource } from "expo-image";
+
+import Button from "@/components/Button";
+import ImageViewer from "@/components/ImageViewer";
 import IconButton from "@/components/IconButton";
+import CircleButton from "@/components/CircleButton";
 import EmojiPicker from "@/components/EmojiPicker";
 import EmojiList from "@/components/EmojiList";
-import { ImageSource } from "expo-image";
 import EmojiSticker from "@/components/EmojiSticker";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
-  const imageRef = useRef<View>(null);
-  const [status, requestPermission] = MediaLibrary.usePermissions();
-  if (status === null) {
-    requestPermission();
-  }
-
   const [selectedImage, setSelectedImage] = useState<string | undefined>(
     undefined
   );
@@ -29,6 +25,12 @@ export default function Index() {
   const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(
     undefined
   );
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+  const imageRef = useRef<View>(null);
+
+  if (status === null) {
+    requestPermission();
+  }
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -57,8 +59,20 @@ export default function Index() {
     setIsModalVisible(false);
   };
 
-  const onSaveImageAsync = () => {
-    alert("Save image");
+  const onSaveImageAsync = async () => {
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        alert("Saved!");
+      }
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
@@ -69,10 +83,10 @@ export default function Index() {
             imgSource={PlaceholderImage}
             selectedImage={selectedImage}
           />
+          {pickedEmoji && (
+            <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+          )}
         </View>
-        {pickedEmoji && (
-          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
-        )}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
